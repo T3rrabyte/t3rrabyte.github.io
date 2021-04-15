9650ef9b0ff8ef84ef8a72be5d38f9025e48bed5
Travis Martin <tjmartin2003@gmail.com>
2021-04-15 15:43:53 -0500

    WIP for Umbra - custom array class and minification.

--- p/experiments/webgl/webgl.js
+++ p/experiments/webgl/webgl.js
@@ -56,6 +56,55 @@ const resizeCanvas = (gl) => {
 	}
 };
 
+const loadTexture = (gl, texture, url) => new Promise((resolve, reject) => {
+	const image = new Image();
+	if ((new URL(url, location.href)).origin != location.origin) { image.crossOrigin = ''; } // Request CORS.
+	image.addEventListener('load', () => {
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+		gl.generateMipmap(gl.TEXTURE_2D);
+		resolve();
+	});
+	image.src = url;
+});
+
+const cubePositions = [
+	1, 1, 1, // 0
+	1, 1, -1, // 1
+	1, -1, 1, // 2
+	1, -1, -1, // 3
+	-1, 1, 1, // 4
+	-1, 1, -1, // 5
+	-1, -1, 1, // 6
+	-1, -1, -1 // 7
+];
+
+const cubeIndices = [
+	// Front
+	6, 4, 2,
+	4, 0, 2,
+
+	// Back
+	3, 1, 7,
+	1, 5, 7,
+
+	// Left
+	7, 5, 6,
+	5, 4, 6,
+
+	// Right
+	2, 0, 3,
+	0, 1, 3,
+
+	// Top
+	7, 6, 3,
+	6, 2, 3,
+
+	// Bottom
+	4, 5, 0,
+	5, 1, 0
+];
+
 class Shader {
 	constructor(gl, src, type) {
 		this.gl = gl;
@@ -98,51 +147,101 @@ class UMath {
 
 	// Convert value in degrees to equivalent value in radians.
 	static degreesToRadians = (d) => d * Math.PI / 180;
+}
 
-	// Create a new array with values based on a rule.
-	static arrayFromRule = (length, rule = (i) => i) => {
-		let output = [];
+class UArray extends Array {
+	// Creates a new array with values based on a rule.
+	static fromRule = (length, rule = (i) => i) => {
+		let output = new Vector();
 		for (let i = 0; i < length; i++) {
 			output[i] = rule(i);
 		}
 		return output;
 	};
+
+	constructor(...data) {
+		super();
+		this.setValues(...data);
+	}
+
+	// Equivalent to "this = []".
+	setValues = (...values) => {
+		while (this.length > 0) {
+			this.pop();
+		}
+		for (let i = 0; i < values.length; i++) {
+			this[i] = values[i];
+		}
+		return this;
+	}
 }
 
-class Matrix {
+class Vector extends UArray {
+	// Find the cross product of this and another vector.
+	cross = (vector) => UArray.fromRule(this.data.length, (i) => {
+		const increment = (num) => num + 1 >= this.length ? 0 : num + 1;
+		i = increment(i);
+		let j = increment(i);
+		return this[i] * vector[j] - this[j] * vector[i];
+	});
+
+	// Perform an operation between two vectors.
+	operate = (vector, operation = (a, b) => a + b) => {
+		for (let i = 0; i < this.length; i++) {
+			this[i] = operation(this[i], vector[i]);
+		}
+		return this;
+	};
+
+	// Normalize vector length to point on unit circle.
+	normalize = () => this.operate(this, (length) => length / Math.sqrt(UMath.sigma(0, this.length - 1, (i) => this[i] * this[i])));
+}
+
+class Matrix extends UArray {
 	// Create an identity matrix for the given dimensions.
-	static identity = (dim = 4) => UMath.arrayFromRule(dim, (x) => UMath.arrayFromRule(dim, (y) => x == y ? 1 : 0));
+	static identity = (dim = 4) => UArray.fromRule(dim, (x) => UArray.fromRule(dim, (y) => x == y ? 1 : 0));
 
 	// Defaults to a four-dimensional identity matrix.
-	constructor(data = Matrix.identity()) {
-		this.data = data;
+	constructor(...data) {
+		super(...(data.length ? data : Matrix.identity()));
 	}
 
 	// Flatten to a one-dimensional array.
-	flatten = () => [].concat(...this.data);
+	// WebGL uses column-wise matrices, so columnWise should be set to true for use with WebGL.
+	flatten = (columnWise) => {
+		const output = [];
+		for (let x = 0; x < this.length; x++) {
+			for (let y = 0; y < this[x].length; y++) {
+				output.push(this[columnWise ? y : x][columnWise ? x : y]);
+			}
+		}
+		return output;
+	};
 
 	// Multiply by another matrix via iterative algorithm.
 	// If C = AB for an (n * m) matrix A and an (m * p) matrix B, then C is an (n * p) matrix with entries.
-	multiply = (b) => {
+	// WebGL uses column-wise matrices, so columnWise should be set to true for use with WebGL.
+	multiply = (matrix, columnWise) => {
 		// A is this
-		// B is b
+		// B is matrix
 		// C is the return value.
 
-		const n = this.data.length;
-		const m = b.data.length;
-		const p = b.data[0].length;
+		const n = this.length;
+		const m = matrix.length;
+		const p = matrix[0].length;
 
-		this.data = UMath.arrayFromRule(n, (i) => UMath.arrayFromRule(p, (j) => UMath.sigma(0, m - 1, (k) => this.data[i][k] * b.data[k][j])));
+		this.setValues(...UArray.fromRule(n, (i) => UArray.fromRule(p, (j) => UMath.sigma(0, m - 1, (k) =>
+			this[columnWise ? k : i][columnWise ? i : k] * matrix[columnWise ? j : k][columnWise ? k : j]))));
 		return this;
 	}
 
 	// Translate by (x, y, z).
-	translate = (x, y, z) => this.multiply(new Matrix([
+	translate = (x, y, z) => this.multiply(new Matrix(
 		[1, 0, 0, 0],
 		[0, 1, 0, 0],
 		[0, 0, 1, 0],
 		[x, y, z, 1]
-	]));
+	), true);
 
 	// Rotate d degrees about the x axis.
 	pitch = (d) => {
@@ -150,12 +249,12 @@ class Matrix {
 		const c = Math.cos(r);
 		const s = Math.sin(r);
 
-		return this.multiply(new Matrix([
+		return this.multiply(new Matrix(
 			[1, 0, 0, 0],
 			[0, c, s, 0],
 			[0, -s, c, 0],
 			[0, 0, 0, 1]
-		]));
+		), true);
 	}
 
 	// Rotate d degrees about the y axis.
@@ -164,12 +263,12 @@ class Matrix {
 		const c = Math.cos(r);
 		const s = Math.sin(r);
 
-		return this.multiply(new Matrix([
+		return this.multiply(new Matrix(
 			[c, 0, -s, 0],
 			[0, 1, 0, 0],
 			[s, 0, c, 0],
 			[0, 0, 0, 1]
-		]));
+		), true);
 	}
 
 	// Rotate d degrees about the z axis.
@@ -178,102 +277,129 @@ class Matrix {
 		const c = Math.cos(r);
 		const s = Math.sin(r);
 
-		return this.multiply(new Matrix([
+		return this.multiply(new Matrix(
 			[c, s, 0, 0],
 			[-s, c, 0, 0],
 			[0, 0, 1, 0],
 			[0, 0, 0, 1]
-		]));
+		), true);
 	}
 
 	// Rotate (x, y, z) degrees.
 	rotate = (x, y, z) => this.pitch(x).yaw(y).roll(z);
 
 	// Scale by (x, y, z) times.
-	scale = (x, y, z) => this.multiply(new Matrix([
+	scale = (x, y, z) => this.multiply(new Matrix(
 		[x, 0, 0, 0],
 		[0, y, 0, 0],
 		[0, 0, z, 0],
 		[0, 0, 0, 1]
-	]));
-}
+	), true);
 
-// Create a matrix which converts screen space to clip space.
-const orthographicMatrix = (left, right, bottom, top, near, far) => [
-	2 / (right - left), 0, 0, 0,
-	0, 2 / (top - bottom), 0, 0,
-	0, 0, 2 / (near - far), 0,
-	(left + right) / (left - right), (bottom + top) / (bottom - top), (near + far) / (near - far), 1
-];
+	// Invert a matrix via Gaussian elimination. Based on work by Andrew Ippoliti. Only possible for square matrices.
+	invert = () => {
+		/*
+		if (this.data.length != this.data[0].length) {
+			return; // Not square.
+		}
+		*/
 
-// Create a matrix which applies perspective to a camera.
-const perspectiveMatrix = (fov = 60, aspectRatio, near = 1, far = 2000) => {
-	const f = Math.tan(Math.PI * 0.5 - 0.5 * degreesToRadians(fov));
-	const range = 1.0 / (near - far);
-	return [
-		f / aspectRatio, 0, 0, 0,
-		0, f, 0, 0,
-		0, 0, (near + far) * range, -1,
-		0, 0, near * far * range * 2, 0
-	];
-};
+		const dim = this.length;
+		const identity = Matrix.identity(dim);
+		const copy = [...this]; // Work with a copy so that failing to execute doesn't ruin the Matrix.
 
-// Invert a matrix via Gaussian elimination. Based on work by Andrew Ippoliti. Only possible for square matrices.
-const invertMatrix = (m) => {
-	const dim = Math.sqrt(m.length);
+		for (let i = 0; i < dim; i++) {
+			let diagonal = copy[i][i]; // Get the element on the diagonal.
 
-	const identity = identityMatrix(dim);
-	m = [...m]; // Duplicate to avoid modifying original.
+			// Swap with a lower row if there is a 0 on the diagonal.
+			if (diagonal == 0) {
+				// Look through every lower row.
+				for (let ii = i + 1; ii < dim; ii++) {
+					// If the row has a non-zero on the correct column, we swap them.
+					if (copy[ii][i] != 0) {
+						for (let j = 0; j < dim; j++) {
+							diagonal = copy[i][j];
+							copy[i][j] = copy[ii][j];
+							copy[ii][j] = diagonal;
 
-	for (let y = 0; y < dim; y++) {
-		let d = m[y * dim + y]; // Get the element on the diagonal.
+							diagonal = identity[i][j];
+							identity[i][j] = identity[ii][j];
+							identity[ii][j] = diagonal;
+						}
 
-		// If there is a 0 on the diagonal, need to swap with a lower row.
-		if (d == 0) {
-			for (let y2 = y + 1; y2 < dim; y2++) {
-				if (m[y2 * dim + y] != 0) {
-					for (let x = 0; x < dim; x++) {
-						d = m[y * dim + x];
-						m[y * dim + x] = m[y2 * dim + x];
-						m[y2 * dim + x] = d;
-
-						d = identity[y * dim + x];
-						identity[y * dim + x] = identity[y2 * dim + x];
-						identity[y2 * dim + x] = d;
+						break;
 					}
+				}
 
-					break;
+				diagonal = copy[i][i];
+
+				if (diagonal == 0) {
+					return; // Matrix is not invertible.
 				}
 			}
 
-			d = m[y * dim + y];
+			// Scale the row by the diagonal so that all diagonals are 1.
+			for (let j = 0; j < dim; j++) {
+				copy[i][j] = copy[i][j] / diagonal;
+				identity[i][j] = identity[i][j] / diagonal;
+			}
 
-			if (d == 0) {
-				return; // Matrix is not invertible.
+			// Subtract the row from all other rows so that there are 0s in this column in other rows.
+			for (let ii = 0; ii < dim; ii++) {
+				if (ii == i) {
+					continue;
+				}
+
+				diagonal = copy[ii][i];
+
+				for (let j = 0; j < dim; j++) {
+					copy[ii][j] -= diagonal * copy[i][j];
+					identity[ii][j] -= diagonal * identity[i][j];
+				}
 			}
 		}
 
-		// Scale the row down so that there is a 1 on the diagonal.
-		for (let x = 0; x < dim; x++) {
-			m[y * dim + x] /= d;
-			identity[y * dim + x] /= d;
-		}
+		// After all operations, copy will be the identity and identity will be the inverse.
+		return this.multiply(new Matrix(...identity), true);
+	};
+}
 
-		// Subtract this row from all of the others.
-		for (let y2 = 0; y2 < dim; y2++) {
-			if (y2 == y) {
-				continue; // Skip current row.
-			}
-
-			d = m[y2 * dim + y];
-
-			for (let x = 0; x < dim; x++) {
-				m[y2 * dim + x] -= d * m[y * dim + x];
-				identity[y2 * dim + x] -= d * identity[y * dim + x];
-			}
-		}
+class Camera extends Matrix {
+	constructor() {
+		super(); // Always construct a camera using the default identity matrix.
 	}
 
-	return identity;
-};
+	// Make camera convert screen space directly to clip space.
+	orthographic = (left, right, bottom, top, near, far) => this.multiply(new Matrix(
+		[2 / (right - left), 0, 0, 0],
+		[0, 2 / (top - bottom), 0, 0],
+		[0, 0, 2 / (near - far), 0],
+		[(left + right) / (left - right), (bottom + top) / (bottom - top), (near + far) / (near - far), 1]
+	));
 
+	// Apply perspective to the camera.
+	perspective = (fov, aspectRatio, near, far) => {
+		const f = Math.tan(Math.PI * 0.5 - 0.5 * UMath.degreesToRadians(fov));
+		const range = 1.0 / (near - far);
+		return this.multiply(new Matrix(
+			[f / aspectRatio, 0, 0, 0],
+			[0, f, 0, 0],
+			[0, 0, (near + far) * range, -1],
+			[0, 0, near * far * range * 2, 0]
+		));
+	}
+
+	// Position camera and face towards a position.
+	lookAt = (position, targetPosition, up = new Vector(0, 1, 0)) => {
+		const zAxis = new Vector(...position).operate(targetPosition, (a, b) => a - b).normalize();
+		const xAxis = up.cross(zAxis).normalize();
+		const yAxis = new Vector(...zAxis).cross(xAxis).normalize();
+
+		return this.multiply(new Matrix(
+			[...xAxis, 0],
+			[...yAxis, 0],
+			[...zAxis, 0],
+			[...position, 1]
+		));
+	};
+}
