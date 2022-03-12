29cfff1f1c76e48fd2963e083486e55b1db322f6
Travis Martin <tjmartin2003@gmail.com>
2022-03-11 22:13:08 -0600

    Minor fixes.

--- components/CodePen.tsx
+++ components/CodePen.tsx
@@ -1,6 +1,13 @@
 import Script from "next/script";
 
-export default function CodePen({ className, height, slugHash, ...props }: any): JSX.Element {
+type Properties = {
+  className: string,
+  height: string,
+  slugHash: string,
+  props: string[]
+};
+
+export default function CodePen({ className, height, slugHash, ...props }: Properties): JSX.Element {
   return (
     <div className={className} {...props}>
       <p className="codepen" data-height={height ? parseInt(height) : 600} data-default-tab="js,result" data-slug-hash={slugHash} />
--- components/MdxArticleLayout.tsx
+++ components/MdxArticleLayout.tsx
@@ -1,7 +1,12 @@
 import "prism-themes/themes/prism-atom-dark.min.css";
 import styles from "../styles/mdx-article.module.scss";
+import { ReactChild } from "react";
 
-export default function MdxArticleLayout({ children }: any): JSX.Element {
+type Properties = {
+  children: ReactChild[]
+}
+
+export default function MdxArticleLayout({ children }: Properties): JSX.Element {
   return (
     <article className={styles["mdx-article"]}>
       {children}
