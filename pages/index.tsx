f57a273597bfe33308011a8717cec6d45ef23a55
Travis Martin <tjmartin2003@gmail.com>
2022-03-16 17:18:50 -0500

    Light theme and better index placeholder.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
 export default function Index(): JSX.Element {
-  return <h1>Placeholder.</h1>
+  return (
+    <>
+      <h1>Travis Martin</h1>
+      <p>This page is currently a placeholder while I work on making it pretty.</p>
+      <p>In the meantime, consider checking out these other cool websites:</p>
+      <ul>
+        <li><a href="https://ty.business">Ty Morrow</a></li>
+        <li><a href="https://griff.pw">Griffon Hansen</a></li>
+        <li><a href="https://xanycki.art">Xanycki</a></li>
+      </ul>
+    </>
+  );
 }
 
 export async function getStaticProps() {
