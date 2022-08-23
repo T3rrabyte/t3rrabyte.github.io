82092ddeffb64502ed524c49493fd4040f998a1c
Travis Martin <tjmartin2003@gmail.com>
2022-08-23 01:39:42 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Index(): JSX.Element {
   return (
     <>
       <h1>Travis Martin</h1>
-      <p>I'm Travis Martin, a junior at the University of Illinois at Chicago, currently pursuing a bachelor's degree in computer science.</p>
+      <p>I am Travis Martin, a junior at the University of Illinois at Chicago, currently pursuing a bachelor&apos;s degree in computer science.</p>
       <p>I have over a decade of programming experience in almost a dozen languages, and I spend much of my free time programming.</p>
       <p>This website is currently undergoing an overhaul to a new framework. In the meantime, please check out my other links:</p>
       <ul>
@@ -10,7 +10,7 @@ export default function Index(): JSX.Element {
         <li><p><a href="https://www.linkedin.com/in/t-j-m/">LinkedIn</a></p></li>
         <li><p><a href="mailto:tjmartin2003@gmail.com">tjmartin2003@gmail.com</a></p></li>
       </ul>
-      <p>If you're looking for my WebGL tutorial series, go <a href="/webgl">here</a>.</p>
+      <p>If you are looking for my WebGL tutorial series, go <a href="/webgl">here</a>.</p>
     </>
   );
 }
