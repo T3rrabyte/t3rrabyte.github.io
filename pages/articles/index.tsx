6af202bc8fc1b251a1769b71133f28acdc649a58
Travis Martin <tjmartin2003@gmail.com>
2022-09-27 23:24:27 -0500

    Articles index.

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,40 +1,25 @@
-import { readdir } from "fs";
-import { promisify } from "util";
-import { join } from "path";
 import Card from "../../assets/components/Card";
 import CardList from "../../assets/components/CardList";
 
-const readDirectory = promisify(readdir);
-
-const pagesBuildDirectory = join(process.cwd(), "pages");
-const articlesWebDirectory = "articles";
-const articlesBuildDirectory = join(pagesBuildDirectory, articlesWebDirectory);
-
-export default function Articles({ slugs }: any) {
+export default function Articles({ pageDatas }: any) {
 	return (
 		<>
 			<h1>Articles</h1>
 			<CardList>
-				{slugs.map((slug: string) =>
-					<Card href={`${articlesWebDirectory}/${slug}`} key={slug}>
-						<h2>{slug}</h2>
-					</Card>
-				)}
+				<Card href="/articles/umbra">
+					<h2>WebGL Tutorial</h2>
+					<p>An entrypoint to my WebGL tutorial.</p>
+				</Card>
 			</CardList>
 		</>
 	);
 }
 
 export async function getStaticProps() {
-	const pages = await readDirectory(articlesBuildDirectory);
-
-	const slugs = pages
-		.filter((fileName) => fileName.match(/\.mdx?$/))
-		.map((fileName) => fileName.split(".")[0]);
-
 	return {
 		props: {
-			slugs
+			title: "Articles",
+			description: "A list of articles that I've written."
 		}
-	}
+	};
 }
