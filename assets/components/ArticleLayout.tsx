2a81bb3ef5109b4e89aee5e1ea92117526c60190
Travis Martin <tjmartin2003@gmail.com>
2022-09-28 01:35:28 -0500

    Uses Next.js links in MDX articles.

--- assets/components/ArticleLayout.tsx
+++ assets/components/ArticleLayout.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 import "katex/dist/katex.min.css";
 import "prism-themes/themes/prism-atom-dark.min.css";
 import styles from "../styles/article.module.scss";
+import Link from "next/link";
 
 const components = {
-	img: ({ ...props }: any) => <span style={{ display: "block", textAlign: "center" }}><Image {...props} /></span>
+	img: ({ alt, src, ...props }) => <span style={{ display: "block", textAlign: "center" }}><Image alt={alt} src={src} {...props} /></span>,
+	a: ({ href, ...props }) => <Link href={href}><a {...props} /></Link>
 };
 
 export default function ArticleLayout({ children }) {
