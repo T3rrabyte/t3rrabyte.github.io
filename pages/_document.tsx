5622d4671d30e94cef4f6435b837c287b504da9c
Travis Martin <tjmartin2003@gmail.com>
2022-09-30 22:14:24 -0500

    Font optimization.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document.js";
 export default function MyDocument() {
 	return (
 		<Html lang="en-US">
-			<Head />
+			<Head>
+				<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu%20Mono&display=optional" rel="stylesheet" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
