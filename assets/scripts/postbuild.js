8fb8b38f83f99321584902df34024015951e34dd
Travis Martin <tjmartin2003@gmail.com>
2022-10-03 03:06:00 -0500

    Use http to make RSS validator happy.

--- assets/scripts/postbuild.js
+++ assets/scripts/postbuild.js
@@ -38,7 +38,7 @@ for (const article of articles) {
 
 // Create XML string for the RSS feed.
 const rssContent = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" xmlns:atom="https://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 	<channel>
 		<title>Travis Martin's Blog</title>
 		<link>${baseUrl}</link>
