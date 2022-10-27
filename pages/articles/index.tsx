12283bd8c940e02511956ff4051bd690c3155673
Travis Martin <tjmartin2003@gmail.com>
2022-10-27 03:56:56 -0500

    Minecraft server article and old serverpacks.

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -3,8 +3,10 @@ import CardList from "../../assets/components/CardList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRss } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import Image from "next/image";
 import { getBuildPaths, getFileName, getFrontMatter, getSlug, getWebPath } from "../../assets/scripts/files";
 import { articlesBuildPath, rssWebPath } from "../../assets/scripts/constants";
+import cardStyles from "../../assets/styles/card.module.scss";
 
 export default function Articles({ articles }) {
 	return (
@@ -14,6 +16,9 @@ export default function Articles({ articles }) {
 				{articles.sort((a, b) => new Date(a.frontMatter.date ?? 0) > new Date(b.frontMatter.date ?? 0) ? -1 : 1).map((article) =>
 					<Card href={article.webPath} key={article.slug}>
 						<h2>{article.frontMatter.title ?? "Untitled Article"}</h2>
+						{article.frontMatter.imageUrl ? <div className={cardStyles["img-container"]}>
+							<Image src={article.frontMatter.imageUrl} layout="fill" alt="Article image." />
+						</div> : null}
 						<p>{article.frontMatter.description ?? "No description."}</p>
 					</Card>
 				)}
