622a5004bff69d6d86c5d286b7c7dbf673b0d1d1
Travis Martin <tjmartin2003@gmail.com>
2022-11-12 12:34:45 -0600

    More oopsie.

--- app/a/webgl/uniforms/head.jsx
+++ app/a/webgl/uniforms/head.jsx
@@ -6,10 +6,10 @@ import CanonicalUrl from "../../../CanonicalUrl";
 export default function Head() {
 	return (
 		<>
-			<Title>WebGL Program Structure</Title>
-			<Description>A reference page for the typical structure of a WebGL API program.</Description>
+			<Title>WebGL Uniforms</Title>
+			<Description>An introduction to uniforms in WebGL.</Description>
 			<ImageUrl>/favicon.png</ImageUrl>
-			<CanonicalUrl>/a/webgl/program-structure</CanonicalUrl>
+			<CanonicalUrl>/a/webgl/uniforms</CanonicalUrl>
 		</>
 	);
 }
