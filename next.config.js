11c98073d1e1b0a78a1470dddcf80e1e90b5fafc
Travis Martin <tjmartin2003@gmail.com>
2023-10-19 14:03:15 -0500

    Update dependencies.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 import nextMdx from "@next/mdx";
-import remarkMath from "remark-math";
+import remarkMath from "remark-math"; // Await update for `@mdx-js/loader` that depends on `unified@11.0.0` to update to `remark-math@6.0.0`.
 import rehypeKatex from "rehype-katex";
 import rehypeHighlight from "rehype-highlight";
 import glsl from "highlight.js/lib/languages/glsl";
