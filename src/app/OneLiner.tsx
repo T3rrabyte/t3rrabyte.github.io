4b08e89e301f58fb44d836c20447a9466e27e6d4
Travis Martin <tjmartin2003@gmail.com>
2024-02-09 01:19:06 -0600

    Music notes on one-liners.

--- src/app/OneLiner.tsx
+++ src/app/OneLiner.tsx
@@ -34,11 +34,11 @@ const oneLiners: Array<JSX.Element> = [
 	<span key={5}>{"There's a frood who really knows where his towel is."}</span>, // The Hitchhiker's Guide to the Galaxy
 	<span key={6}>{"So Long, and Thanks for All the Fish."}</span>, // The Hitchhiker's Guide to the Galaxy
 	<span key={7}>{"Roads? Where we're going, we don't need roads!"}</span>, // Back to the Future
-	<span key={8}>{"Trigger, killer, eye of the storm."}</span>, // Zero Eclipse; Attack on Titan
+	<span key={8}>{"♪ Trigger, killer, eye of the storm ♪"}</span>, // Zero Eclipse; Attack on Titan
 	<span key={9}>{"Rich Purnell is a steely-eyed missile man!"}</span>, // The Martian
 	<span key={10}>{"Death awaits you all with big, nasty, pointy teeth!"}</span>, // The Killer Rabbit of Caerbannog; Monty Python and the Holy Grail
 	<span key={11}>{"What was, shall be. What shall be, was."}</span>, // The Worm-in-Waiting; Horizon Signal; Stellaris
-	<span key={12}>{"There's blood on the crown, go and take it."}</span>, // Rise; League of Legends
+	<span key={12}>{"♪ There's blood on the crown, go and take it ♪"}</span>, // Rise; League of Legends
 	<span key={13}>{"It's dangerous to go alone!"}</span>, // The Legend of Zelda
 	<span key={14}>{"^ ^ v v < > < > B A S"}</span>, // The Konami Code
 	<span key={15}>
@@ -61,7 +61,7 @@ const oneLiners: Array<JSX.Element> = [
 	<span key={30}>{"There is no spoon."}</span>, // The Matrix
 	<span key={31}>{"Shaken, not stirred."}</span>, // James Bond
 	<span key={32}>{"Come with me if you want to live."}</span>, // Terminator
-	<span key={33}>{"Maybe it's a dream, maybe nothing else is real."}</span>, // Bad Apple!!; Touhou Project
+	<span key={33}>{"♪ Maybe it's a dream, maybe nothing else is real ♪"}</span>, // Bad Apple!!; Touhou Project
 	<span key={34}>{"Elementary, my dear Watson."}</span>, // Sherlock Holmes
 	<span key={35}>{"Have fun storming the castle!"}</span>, // The Princess Bride
 	<span key={36}>{"Houston, we have a problem."}</span>, // Apollo 13; NASA
@@ -71,7 +71,7 @@ const oneLiners: Array<JSX.Element> = [
 	<span key={30}>{"May the Shwartz be with you."}</span>, // Spaceballs
 	<span key={41}>{"If you can dodge a wrench, you can dodge a ball."}</span>, // Dodgeball: A True Underdog Story
 	<span key={42}>{"Open the pod bay doors, HAL."}</span>, // 2001: A Space Odyssey
-	<span key={43}>{"A little bit of everything, all of the time."}</span>, // Welcome to the Internet; Bo Burnham; Inside
+	<span key={43}>{"♪ A little bit of everything, all of the time ♪"}</span>, // Welcome to the Internet; Bo Burnham; Inside
 	<span key={44}>
 		<DynamicLink href="https://discord.gg/sdcleague">{"SDC"}</DynamicLink>
 		{" champion."}
@@ -79,7 +79,7 @@ const oneLiners: Array<JSX.Element> = [
 	<span key={45}>{"Time favors the Thallids."}</span>, // Magic: The Gathering
 	<span key={46}>{"WD-40 Fan"}</span>, // Inside joke
 	<span key={47}>
-		{"Another way to feel what you didn't want yourself to know."}
+		{"♪ Another way to feel what you didn't want yourself to know ♪"}
 	</span>, // I Really Want to Stay at Your House; Cyberpunk: Edgerunners
 	<code key={48}>{":(){ :|:&};:"}</code>, // Shell fork bomb
 	<span key={49}>{"Excelsior!"}</span>, // "Ever upward!"; Stan Lee; Marvel
@@ -90,17 +90,20 @@ const oneLiners: Array<JSX.Element> = [
 	</span>, // The Dandelion Girl; Clannad
 	<span key={51}>{"El Psy Kongroo"}</span>, // Steins;Gate
 	<span key={52}>
-		{"If everyone is not special, maybe you can be what you want to be."}
+		{"♪ If everyone is not special, maybe you can be what you want to be ♪"}
 	</span>, // 99; MOB CHOIR; Mob Psycho 100
 	<span key={53}>{"God drinks Java."}</span>, // world.execute(me);; Mili
 	<span key={54}>{"SEE YOU SPACE COWBOY..."}</span>, // Cowboy Bebop
 	<span key={55}>{"Dodge, duck, dip, dive, and dodge."}</span>, // Dodgeball: A True Underdog Story
-	<span key={56}>{'I\'m always dreaming, believing "ideal" exists.'}</span>, // Thousand Absolutes; DEMONDICE, Omega Strikers
-	<span key={57}>{"My naked heart can hear echoes, echoes."}</span>, // Avid; Hiroyuki Sawano; 86 EIGHTY-SIX
-	<span key={58}>{"I turn the page to drown you out."}</span>, // Voices of the Chord; Hiroyuki Sawano; 86 EIGHTY-SIX
+	<span key={56}>{'♪ I\'m always dreaming, believing "ideal" exists ♪'}</span>, // Thousand Absolutes; DEMONDICE, Omega Strikers
+	<span key={57}>{"♪ My naked heart can hear echoes, echoes ♪"}</span>, // Avid; Hiroyuki Sawano; 86 EIGHTY-SIX
+	<span key={58}>{"♪ I turn the page to drown you out ♪"}</span>, // Voices of the Chord; Hiroyuki Sawano; 86 EIGHTY-SIX
 	<span key={59}>{"you cannot kill me in a way that matters"}</span>, // Mushroom shitpost
 	<span key={60}>{"There can be only one."}</span>, // Highlander
-	<span key={61}>{"Greed is unlimited; freedom is a limited resource."}</span> // Iron Lotus; Mili; Library of Ruina
+	<span key={61}>
+		{"♪ Greed is unlimited; freedom is a limited resource ♪"}
+	</span>, // Iron Lotus; Mili; Library of Ruina
+	<span key={62}>{"♪ Some little ironies called tragedies so silently ♪"}</span> // Icon; Hiroyuki Sawano; Kabaneri of the Iron Fortress
 ];
 
 export default function OneLiner(
