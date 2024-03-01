5101ee1c3a2699bdc5407581b0796339be0b4987
Travis Martin <tjmartin2003@gmail.com>
2024-03-01 05:30:56 -0600

    Remove unused imports.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,6 @@
 import { BsRss } from "react-icons/bs";
 import CardList from "#CardList";
 import Card from "#Card";
-import Image from "next/image";
-import minecraftServerPreview from "#app/a/mc/preview.png";
 import SpecularLighting from "#app/a/webgl/lighting/SpecularLighting.tsx";
 import generateMetadata from "#generateMetadata";
 import type { Metadata } from "next";
@@ -20,15 +18,24 @@ export default function page(): JSX.Element {
 			<CardList>
 				<Card href="/a/pedit5">
 					<h2>The Dungeon</h2>
-					<p>An in-depth look at the first computer role-playing game, including a review of my world record speedrun.</p>
+					<p>
+						An in-depth look at the first computer role-playing game, including
+						a review of my world record speedrun.
+					</p>
 				</Card>
 				<Card href="/a/q_rsqrt">
 					<h2>Fast Inverse Square Root</h2>
-					<p>An explanation of the famous fast inverse square root algorithm as it is implemented in Quake III Arena.</p>
+					<p>
+						An explanation of the famous fast inverse square root algorithm as
+						it is implemented in Quake III Arena.
+					</p>
 				</Card>
 				<Card href="/a/esojs">
 					<h2>Esoteric JavaScript</h2>
-					<p>A guide on how to write any JavaScript program with just six unique characters.</p>
+					<p>
+						A guide on how to write any JavaScript program with just six unique
+						characters.
+					</p>
 				</Card>
 				<Card href="/a/webgl">
 					<h2>WebGL2 Tutorial</h2>
