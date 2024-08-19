93bfad396b5cb1e3a4dc86e87468b864e6592ceb
Travis Martin <tjmartin2003@gmail.com>
2024-08-19 03:36:33 -0500

    Fix domain name.

--- src/scripts/domain.ts
+++ src/scripts/domain.ts
@@ -1,13 +1,8 @@
 // This should be equivalent to `window.location.origin`, but it can be used on both the server and the client.
 
-const vercelUrl = process.env["VERCEL_URL"];
-const port = process.env["PORT"] ?? "3000";
-
 const domain =
-	typeof vercelUrl === "string"
-		? `https://${vercelUrl}`
-		: process.env.NODE_ENV === "development"
-			? `http://localhost:${port}`
-			: "https://www.lakuna.pw";
+	process.env["VERCEL_ENV"] === "development"
+		? `http://localhost:${process.env["PORT"] ?? "3000"}`
+		: "https://www.lakuna.pw";
 
 export default domain;
